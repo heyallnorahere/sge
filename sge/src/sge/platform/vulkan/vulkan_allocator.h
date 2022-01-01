a563e8dc496c189f081807cd2bccb5a963ad8c0e
Nora Beda <theo.github@bedafamily.com>
2021-12-31 22:30:08 -0800

    happy new year

--- sge/src/sge/platform/vulkan/vulkan_allocator.h
+++ sge/src/sge/platform/vulkan/vulkan_allocator.h
@@ -1,5 +1,5 @@
 /*
-   Copyright 2021 Nora Beda
+   Copyright 2022 Nora Beda and SGE contributors
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
