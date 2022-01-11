205fe00cd11ee133908a0a30199c73800b74a933
yodasoda1219 <theo.github@bedafamily.com>
2022-01-11 11:47:58 -0800

    sandbox testbed maybe?

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -18,12 +18,13 @@
 #include <sge.h>
 #include <sge/renderer/renderer.h>
 #include <sge/renderer/framebuffer.h>
+using namespace sge;
 namespace sandbox {
-    class camera_controller : public sge::entity_script {
+    class camera_controller : public entity_script {
     public:
-        virtual void on_update(sge::timestep ts) {
-            if (sge::input::get_mouse_button(sge::mouse_button::left)) {
-                glm::vec2 mouse_position = sge::input::get_mouse_position();
+        virtual void on_update(timestep ts) {
+            if (input::get_mouse_button(mouse_button::left)) {
+                glm::vec2 mouse_position = input::get_mouse_position();
                 if (!m_last_mouse_position.has_value()) {
                     m_last_mouse_position = mouse_position;
                 }
@@ -32,36 +33,36 @@ namespace sandbox {
                     (mouse_position - m_last_mouse_position.value()) * glm::vec2(1.f, -1.f);
                 m_last_mouse_position = mouse_position;
 
-                auto window = sge::application::get().get_window();
+                auto window = application::get().get_window();
                 uint32_t width = window->get_width();
                 uint32_t height = window->get_height();
 
                 glm::vec2 window_size = glm::vec2((float)width, (float)height);
                 float aspect_ratio = window_size.x / window_size.y;
 
-                auto& camera_data = get_component<sge::camera_component>();
+                auto& camera_data = get_component<camera_component>();
                 float camera_view_size = camera_data.camera.get_orthographic_size();
                 glm::vec2 view_size = glm::vec2(camera_view_size * aspect_ratio, camera_view_size);
 
-                auto& transform = get_component<sge::transform_component>();
+                auto& transform = get_component<transform_component>();
                 transform.translation -= offset * view_size / window_size;
             } else {
                 m_last_mouse_position.reset();
             }
         }
 
-        virtual void on_event(sge::event& event) {
-            sge::event_dispatcher dispatcher(event);
+        virtual void on_event(event& e) {
+            event_dispatcher dispatcher(e);
 
-            dispatcher.dispatch<sge::mouse_scrolled_event>(
+            dispatcher.dispatch<mouse_scrolled_event>(
                 SGE_BIND_EVENT_FUNC(camera_controller::on_scroll));
         }
 
     private:
-        bool on_scroll(sge::mouse_scrolled_event& event) {
-            auto& camera_data = get_component<sge::camera_component>();
+        bool on_scroll(mouse_scrolled_event& e) {
+            auto& camera_data = get_component<camera_component>();
             float view_size = camera_data.camera.get_orthographic_size();
-            view_size -= event.get_offset().y;
+            view_size -= e.get_offset().y;
             camera_data.camera.set_orthographic_size(view_size);
 
             return true;
@@ -70,117 +71,126 @@ namespace sandbox {
         std::optional<glm::vec2> m_last_mouse_position;
     };
 
-    class penguin_script : public sge::entity_script {
+    class collision_listener_script : public entity_script {
     public:
-        virtual void on_collision(sge::entity other) override {
-            auto& tag = other.get_component<sge::tag_component>();
-            spdlog::info("tux: collided with {0}", tag.tag);
+        virtual void on_collision(entity other) override {
+            auto& my_tag = get_component<tag_component>();
+            auto& other_tag = other.get_component<tag_component>();
+            spdlog::info("{0}: collided with {1}", my_tag.tag, other_tag.tag);
         }
     };
 
-    class sandbox_layer : public sge::layer {
+    class sandbox_layer : public layer {
     public:
         sandbox_layer() : layer("Sandbox Layer") {}
 
         virtual void on_attach() override {
-            auto image_data = sge::image_data::load("assets/images/tux.png");
-            auto img = sge::image_2d::create(image_data, sge::image_usage_texture);
+            auto image_data = image_data::load("assets/images/tux.png");
+            auto img = image_2d::create(image_data, image_usage_texture);
 
-            sge::texture_spec tex_spec;
+            texture_spec tex_spec;
             tex_spec.image = img;
-            tex_spec.filter = sge::texture_filter::linear;
-            tex_spec.wrap = sge::texture_wrap::repeat;
-            this->m_tux = sge::texture_2d::create(tex_spec);
+            tex_spec.filter = texture_filter::linear;
+            tex_spec.wrap = texture_wrap::repeat;
+            m_tux = texture_2d::create(tex_spec);
 
-            auto window = sge::application::get().get_window();
+            auto window = application::get().get_window();
             uint32_t width = window->get_width();
             uint32_t height = window->get_height();
 
-            this->m_scene = sge::ref<sge::scene>::create();
-            this->m_scene->set_viewport_size(width, height);
+            m_scene = ref<scene>::create();
+            m_scene->set_viewport_size(width, height);
 
             // Tux
             {
-                this->m_entity = this->m_scene->create_entity("Penguin");
-                
-                auto& transform = this->m_entity.get_component<sge::transform_component>();
+                entity tux = m_scene->create_entity("Penguin");
+
+                auto& transform = tux.get_component<transform_component>();
                 transform.scale = glm::vec2(5.f);
                 transform.rotation = 10.f;
 
-                auto& sprite = this->m_entity.add_component<sge::sprite_renderer_component>();
+                auto& sprite = tux.add_component<sprite_renderer_component>();
                 sprite.texture = this->m_tux;
 
-                auto& nsc = this->m_entity.add_component<sge::native_script_component>();
-                nsc.bind<penguin_script>();
+                auto& nsc = tux.add_component<native_script_component>();
+                nsc.bind<collision_listener_script>();
 
-                auto& rb = this->m_entity.add_component<sge::rigid_body_component>();
-                rb.type = sge::rigid_body_component::body_type::dynamic;
+                auto& rb = tux.add_component<rigid_body_component>();
+                rb.type = rigid_body_component::body_type::dynamic;
 
-                auto& bc = this->m_entity.add_component<sge::box_collider_component>();
+                auto& bc = tux.add_component<box_collider_component>();
                 bc.restitution = 0.8f;
             }
 
             // Ground
             {
-                this->m_ground = this->m_scene->create_entity("Ground");
-                auto& transform = this->m_ground.get_component<sge::transform_component>();
+                entity ground = m_scene->create_entity("Ground");
+
+                auto& transform = ground.get_component<transform_component>();
                 transform.scale = glm::vec2(100.f, 1.f);
                 transform.translation = glm::vec2(0.f, -10.f);
-                auto& sprite = this->m_ground.add_component<sge::sprite_renderer_component>();
+
+                auto& sprite = ground.add_component<sprite_renderer_component>();
                 sprite.color = glm::vec4(0.04f, 0.45f, 0.19f, 1.f);
-                auto& rb = this->m_ground.add_component<sge::rigid_body_component>();
-                rb.type = sge::rigid_body_component::body_type::static_;
-                auto& bc = this->m_ground.add_component<sge::box_collider_component>();
+
+                auto& rb = ground.add_component<rigid_body_component>();
+                rb.type = rigid_body_component::body_type::static_;
+
+                ground.add_component<box_collider_component>();
+
+                auto& nsc = ground.add_component<native_script_component>();
+                nsc.bind<collision_listener_script>();
             }
 
             // Camera
             {
-                this->m_camera = this->m_scene->create_entity("Camera");
-                auto& cc = this->m_camera.add_component<sge::camera_component>();
-                cc.camera.set_orthographic_size(15.f);
-                this->m_camera.add_component<sge::native_script_component>()
-                    .bind<camera_controller>();
+                entity camera = m_scene->create_entity("Camera");
 
-                auto& transform = this->m_camera.get_component<sge::transform_component>();
+                auto& cc = camera.add_component<camera_component>();
+                cc.camera.set_orthographic_size(15.f);
+
+                auto& nsc = camera.add_component<native_script_component>();
+                nsc.bind<camera_controller>();
+
+                auto& transform = camera.get_component<transform_component>();
                 transform.translation = glm::vec2(0.f, -5.f);
             }
 
-            this->m_scene->on_start();
+            m_scene->on_start();
         }
 
         virtual void on_detach() override {
-            this->m_scene->on_stop();
-            this->m_scene.reset();
-            this->m_tux.reset();
+            m_scene->on_stop();
+            m_scene.reset();
+            m_tux.reset();
         }
 
-        virtual void on_event(sge::event& event) override {
-            sge::event_dispatcher dispatcher(event);
+        virtual void on_event(event& e) override {
+            event_dispatcher dispatcher(e);
 
-            dispatcher.dispatch<sge::window_resize_event>(
-                SGE_BIND_EVENT_FUNC(sandbox_layer::on_resize));
+            dispatcher.dispatch<window_resize_event>(SGE_BIND_EVENT_FUNC(sandbox_layer::on_resize));
 
-            if (!event.handled) {
-                this->m_scene->on_event(event);
+            if (!e.handled) {
+                m_scene->on_event(e);
             }
         }
 
-        virtual void on_update(sge::timestep ts) override { this->m_scene->on_runtime_update(ts); }
+        virtual void on_update(timestep ts) override { m_scene->on_runtime_update(ts); }
 
         virtual void on_imgui_render() override {
             ImGui::Begin("Sandbox");
 
             ImGui::Text("Hello!");
-            ImGui::Image(this->m_tux->get_imgui_id(), ImVec2(50.f, 50.f));
+            ImGui::Image(m_tux->get_imgui_id(), ImVec2(50.f, 50.f));
 
             ImGui::End();
         }
 
     private:
-        bool on_resize(sge::window_resize_event& event) {
-            uint32_t width = event.get_width();
-            uint32_t height = event.get_height();
-            this->m_scene->set_viewport_size(width, height);
+        bool on_resize(window_resize_event& e) {
+            uint32_t width = e.get_width();
+            uint32_t height = e.get_height();
+            m_scene->set_viewport_size(width, height);
 
             if (width == 0 || height == 0) {
                 spdlog::info("window was minimized");
@@ -193,28 +203,27 @@ namespace sandbox {
 
         using seconds_t = std::chrono::duration<double>;
         seconds_t m_time_record = seconds_t(0);
-        sge::ref<sge::scene> m_scene;
-        sge::ref<sge::texture_2d> m_tux;
-        sge::entity m_entity, m_camera, m_ground;
+        ref<scene> m_scene;
+        ref<texture_2d> m_tux;
     };
 
-    class sandbox_app : public sge::application {
+    class sandbox_app : public application {
     public:
         sandbox_app() : application("Sandbox") {}
 
     protected:
         virtual void on_init() override {
-            this->m_layer = new sandbox_layer;
-            this->push_layer(this->m_layer);
+            m_layer = new sandbox_layer;
+            push_layer(m_layer);
         }
 
         virtual void on_shutdown() override {
-            this->pop_layer(this->m_layer);
-            delete this->m_layer;
+            pop_layer(m_layer);
+            delete m_layer;
         }
 
         sandbox_layer* m_layer;
     };
 } // namespace sandbox
 
-sge::application* create_app_instance() { return new sandbox::sandbox_app; };
+application* create_app_instance() { return new sandbox::sandbox_app; };
